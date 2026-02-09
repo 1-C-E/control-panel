ce33faa96fde95dad76d064b9cd9fe82eabea68c
1-C-E <pitenevv@mail.ru>
2026-02-10 00:21:43 +0300

    created the basis for the development

--- src/app/ui/App.tsx
+++ src/app/ui/App.tsx
@@ -1,10 +1,13 @@
-import './../ui/styles/App.scss'
+import '@app/ui/styles/App.scss'
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
 	return (
-		<div>
-			<div>fff</div>
-		</div>
+		<ChakraProvider>
+			<div>
+				<div>fff</div>
+			</div>
+		</ChakraProvider>
 	)
 }
 
--- src/app/ui/main.tsx
+++ src/app/ui/main.tsx
@@ -1,6 +1,6 @@
+import '@app/ui/styles/index.scss'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './../ui/styles/index.scss'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
